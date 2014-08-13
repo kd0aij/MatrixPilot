2d3742c2d2b6bbc26d468329c988ebcf4e9752cb
Mark Whitehorn <kd0aij@gmail.com>
2014-08-13 10:06:40 -0600

    branch _mw4fbw: project Polaris-auav3: increase mavlink raw_imu stream rate

--- MatrixPilot/Polaris-auav3/projectSpecific/mavlink_options.h
+++ MatrixPilot/Polaris-auav3/projectSpecific/mavlink_options.h
@@ -26,8 +26,8 @@
 // Common data rates
 #define MAVLINK_RATE_RC_CHAN                0   // RC_CHANNELS_SCALED, RC_CHANNELS_RAW, SERVO_OUTPUT_RAW |
 #define MAVLINK_RATE_IMU_RAW                0   // ATTITUDE_CONTROLLER_OUTPUT, POSITION_CONTROLLER_OUTPUT, NAV_CONTROLLER_OUTPUT. |
-#define MAVLINK_RATE_POSITION               8   // LOCAL_POSITION, GLOBAL_POSITION/GLOBAL_POSITION_INT messages. |
-#define MAVLINK_RATE_RAW_SENSORS            2   // IMU_RAW, GPS_RAW, GPS_STATUS packets.
+#define MAVLINK_RATE_POSITION               1   // LOCAL_POSITION, GLOBAL_POSITION/GLOBAL_POSITION_INT messages. |
+#define MAVLINK_RATE_RAW_SENSORS            100   // IMU_RAW, GPS_RAW, GPS_STATUS packets.
 
 // Fixed data rates
 #define MAVLINK_RATE_HEARTBEAT              4
@@ -40,7 +40,7 @@
 // Send VFR_HUD message at position rate, 1=yes, 0=no.  Needed for correct mavproxy state
 #define MSG_VFR_HUD_WITH_POSITION           1
 
-#define MAVLINK_FRAME_FREQUENCY             40
+#define MAVLINK_FRAME_FREQUENCY             100
 #define MAVLINK_WAYPOINT_TIMEOUT            120 // Dependent on frequency of calling mavlink_output_40hz. 120 is 3 second timeout.
 
 // 19200,38400,57600,115200,230400,460800,921600
